a290a28d57bb30db17920306be6afacdf7244e7c
Default User <laurence.a.maddox@gmail.com>
2017-08-30 13:56:59 -0500

    backing up data before switching from unstable dev box

--- openssl.sh
+++ openssl.sh
@@ -17,6 +17,7 @@ test ! -f $KEY
 test ! -f $CRT
 
 openssl genpkey -out $KEY -algorithm RSA -pkeyopt rsa_keygen_bits:16384
+chmod 0400 $KEY
 openssl req -key $KEY -new -out $CSR
 if [ x$OP == xroot ] ; then
 	openssl ca -in $CSR -out $CRT -selfsign -extensions v3_ca
@@ -24,3 +25,4 @@ elif [ x$OP == xsign ] ; then
 	openssl ca -in $CSR -out $CRT -extensions v3_ca
 else exit 2
 fi
+chmod 0444 $CRT
