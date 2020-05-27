a4cefe5a18303ee67c76b0821f5e956de3e09954
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-26 21:14:25 -0500

    updated reame

--- ssh/ssh-moduli.sh
+++ ssh/ssh-moduli.sh
@@ -3,6 +3,8 @@ set -exu
 
 BITS=${1:-16384}
 
+A=${A:-0}
+
 DIR=`mktemp -d`
 trap "rm -rf $DIR" 0
 cd $DIR
@@ -12,16 +14,21 @@ while : ; do
     ssh-keygen -G moduli-$BITS.candidates -b $BITS
     #ssh-keygen -T moduli-$BITS -f moduli-$BITS.candidates
 
-    #for k in 2 3 5 ; do
-    #  ssh-keygen -T moduli-$BITS-$k -f moduli-$BITS.candidates -a $k ||
-    #  continue 3
-    #done
-    ssh-keygen -T moduli-$BITS-$k -f moduli-$BITS.candidates -a $k ||
+    if [[ "$A" ]] ; then
+    for k in 2 3 5 ; do
+      ssh-keygen -T moduli-$BITS-$k -f moduli-$BITS.candidates -a $k ||
+      continue 3
+    done
+    else
+    ssh-keygen -T moduli-$BITS -f moduli-$BITS.candidates ||
     continue 2
+    fi
     break
   done
 
+  if [[ "$A" ]] ; then
   comms moduli-$BITS-{2,3,5} > moduli-$BITS
+  fi
 
   (( `stat --format=%s moduli-$BITS` == 0 )) ||
   break
