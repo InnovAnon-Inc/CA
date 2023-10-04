6cbb28777e9a7440fd048a98dc08a13dab861b15
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2023-10-04 14:27:44 -0500

    update

--- bin/ca.sh
+++ bin/ca.sh
@@ -10,13 +10,15 @@ fi
 . ./lib/check.env
 . ./lib/cmds.env
 
-case "$0" in
+CMD="$(basename "$0")"
+
+case "$CMD" in
 init_ca|root_ca|nuke)
 if (( $# )) ; then
 echo usage: $0 1>&2
 exit 1
 fi
-"$0"
+"$CMD"
 ;;
 
 intermediate_ca|site|revoke)
@@ -26,7 +28,7 @@ usage: $0 <ca_name> <site_name>
 EOF
 exit 1
 fi
-"$0" "$@"
+"$CMD" "$@"
 ;;
 
 *) cat 1>&2 << EOF
